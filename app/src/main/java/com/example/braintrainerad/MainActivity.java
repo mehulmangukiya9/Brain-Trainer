6323c2a4d529767acfc585aa83fc49088713b1c1
Phd-257 <hdpatel.djp257@gmail.com>
2021-01-15 17:14:46 -0500

    main activity has been updated

--- app/src/main/java/com/example/braintrainerad/MainActivity.java
+++ app/src/main/java/com/example/braintrainerad/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.braintrainerad;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.MenuItem;
@@ -14,10 +20,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.example.braintrainerad.models.Scorecard;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -32,328 +38,130 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-        FirebaseFirestore db;
+    DrawerLayout drawer;
+    Toolbar toolbar;
+    TextView nameTv;
+    EditText nameOfPlayer;
+    TextView levelTv;
+    Spinner levelSpinner;
+    String nameOfaPlayer;
+    Button startButton;
+    String levelChosen;
+    NavigationView nav_view;
+    String[] items;
+    ArrayAdapter<String> adapter;
 
-        DatabaseHelper mydb = new DatabaseHelper(this);
-        private Toolbar toolbar;
-        Button startButton;
-        TextView resultTextView;
-        TextView pointsTextView;
-        Button button0;
-        Button button1;
-        Button button2;
-        Button button3;
-        TextView sumTextView;
-        TextView timerTextView;
-        Button playAgainButton;
-        RelativeLayout gameRelativeLayout;
-        boolean active=true;
-        DrawerLayout drawerLayout;
-        NavigationView navigationView;
-        EditText nameOfPlayer;
-        String nameOfaPlayer;
-        int sum;
-        Spinner dropdown;
-        String levelChoosen;
-        String[] items;
-        ArrayAdapter<String> adapter;
-        int a;
-        int b;
-        TextView nameTv;
-        TextView levelTv;
-        String lastDigit;
+    //new
+    SeekBar secondSeekBar;
+    TextView secondTv;
+    TextView showSecond;
+    String showSec;
 
-        String StringOfIncorrecAns;
 
-        ArrayList<Integer> answers = new ArrayList<Integer>();
-        int locationOfCorrectAnswer;
-        int score = 0;
-        int numberOfQuestions = 0;
 
-        public void viewData(){
-            Cursor c = mydb.getScore();
-            if(c.getCount() <= 0)
-            {
-                Toast.makeText(this, "No Records Found", Toast.LENGTH_SHORT).show();
-            }
-            else
-            {
-                StringBuffer buffer = new StringBuffer();
-                while (c.moveToNext()){
-                    buffer.append("No. "+ c.getString(0)+"\n");
-                    buffer.append("Player Name:"+ c.getString(1)+"\n");
-                    buffer.append("Score:"+ c.getString(2)+"\n\n");
-                    showMsg("Data", buffer.toString());
+    public void start1(View view) {
 
-                }
-            }
-        }
-        public void showMsg(String title, String msg){
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setCancelable(true);
-            builder.setTitle(title);
-            builder.setMessage(msg);
-            builder.show();
+
+        levelChosen = levelSpinner.getSelectedItem().toString();
+        nameOfaPlayer = nameOfPlayer.getText().toString();
+        showSec = showSecond.getText().toString().substring(0,2);
+
+        if(nameOfaPlayer.isEmpty()||levelChosen=="Select The Level--"){
+            Toast.makeText(this, "please enter your name", Toast.LENGTH_SHORT).show();
         }
 
-        public void playAgain(View view) {
+        else {
+            //levelChoosen = dropdown.getSelectedItem().toString();
 
-            active=true;
-            score = 0;
-            dropdown.setVisibility(View.INVISIBLE);
-            numberOfQuestions = 0;
-            toolbar.setTitle("let's start the game");
-            button0.setVisibility(View.VISIBLE);
-            button1.setVisibility(View.VISIBLE);
-            button2.setVisibility(View.VISIBLE);
-            button3.setVisibility(View.VISIBLE);
-            sumTextView.setVisibility(View.VISIBLE);
-            timerTextView.setText("30s");
-            pointsTextView.setText("0/0");
-            resultTextView.setText("");
-            playAgainButton.setVisibility(View.INVISIBLE);
-
-            generateQuestion();
+            Intent gotoGenerateQuestion= new Intent(this,GenerateQuestion.class);
+            gotoGenerateQuestion.putExtra("levelChosen",levelChosen);
+            gotoGenerateQuestion.putExtra("nameOfaPlayer",nameOfaPlayer);
+            gotoGenerateQuestion.putExtra("howManySeconds",showSec);
 
 
-            new CountDownTimer(30100, 1000) {
-
-                @Override
-                public void onTick(long millisUntilFinished) {
-
-                    timerTextView.setText(String.valueOf(millisUntilFinished / 1000) + "s");
-
-                }
-
-                @Override
-                public void onFinish() {
-                    button0.setVisibility(View.INVISIBLE);
-                    button1.setVisibility(View.INVISIBLE);
-                    button2.setVisibility(View.INVISIBLE);
-                    button3.setVisibility(View.INVISIBLE);
-                    sumTextView.setVisibility(View.INVISIBLE);
-                    playAgainButton.setVisibility(View.VISIBLE);
-                    timerTextView.setText("0s");
-                    toolbar.setTitle("Your Score");
-
-
-
-                    boolean isInserted = mydb.insert(nameOfaPlayer, score);
-                    if(isInserted == true)
-                    {
-                        Toast.makeText(MainActivity.this, "Score has been updated", Toast.LENGTH_SHORT).show();
-                    }
-                    else
-                    {
-                        Toast.makeText(MainActivity.this, "Score has been updated", Toast.LENGTH_SHORT).show();
-                    }
-                    resultTextView.setText(nameOfaPlayer+" Your score: " + Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
-                    addScore(nameOfaPlayer, score);
-                    active=false;
-
-                }
-            }.start();
-        }
-
-
-
-
-        public void generateQuestion() {
-
-            if(active) {
-                Random rand = new Random();
-
-                if(levelChoosen=="Easy"){
-
-                    a = rand.nextInt(20)+1;
-                    b = rand.nextInt(20)+1;
-
-                }
-                else if(levelChoosen=="Medium"){
-                    a = rand.nextInt(80-40)+40+1;
-                    b = rand.nextInt(80-40)+40+1;
-
-                }
-                else{
-                    a = rand.nextInt(200-100)+100+1;
-                    b = rand.nextInt(200-100)+100+1;
-                }
-
-
-
-
-
-                sum=a+b;
-                lastDigit = Integer.toString(sum);
-
-                sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
-
-                locationOfCorrectAnswer = rand.nextInt(4);
-
-                answers.clear();
-
-                int incorrectAnswer;
-
-                for (int i = 0; i < 4; i++) {
-
-                    if (i == locationOfCorrectAnswer) {
-
-                        answers.add(a + b);
-
-                    } else {
-
-                        incorrectAnswer = rand.nextInt((sum+7)-(sum-7))+(sum-7)+1;
-
-                        while (incorrectAnswer == a + b || incorrectAnswer==0) {
-
-                            incorrectAnswer = rand.nextInt((sum+7)-(sum-7))+(sum-7)+1;
-
-                        }
-
-                        answers.add(incorrectAnswer);
-
-                    }
-
-                }
-
-                button0.setText(Integer.toString(answers.get(0)));
-                button1.setText(Integer.toString(answers.get(1)));
-                button2.setText(Integer.toString(answers.get(2)));
-                button3.setText(Integer.toString(answers.get(3)));
-
-
-            }
+            startActivity(gotoGenerateQuestion);
 
         }
-
-        public void chooseAnswer(View view) {
-
-            if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
-
-                score++;
-                resultTextView.setText("Correct!");
-
-            } else {
-
-                resultTextView.setText("Wrong!");
-
-            }
-
-            numberOfQuestions++;
-            pointsTextView.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
-            generateQuestion();
-
-
-        }
-
-        public void start(View view) {
-
-
-
-            levelChoosen = dropdown.getSelectedItem().toString();
-            nameOfaPlayer = nameOfPlayer.getText().toString();
-            if(nameOfaPlayer.isEmpty()||levelChoosen=="select the level--"){
-                Toast.makeText(this, "please enter your name", Toast.LENGTH_SHORT).show();
-
-            }
-            else {
-                //levelChoosen = dropdown.getSelectedItem().toString();
-                startButton.setVisibility(View.INVISIBLE);
-                gameRelativeLayout.setVisibility(RelativeLayout.VISIBLE);
-                nameOfPlayer.setVisibility(View.INVISIBLE);
-                playAgain(findViewById(R.id.playAgainButton));
-                dropdown.setVisibility(View.INVISIBLE);
-                nameTv.setVisibility(View.INVISIBLE);
-                levelTv.setVisibility(View.INVISIBLE);
-            }
-        }
-
-    public void addScore(String nameOfaPlayer, int score){
-        Scorecard scorecard = new Scorecard(nameOfaPlayer, score);
-
-        db.collection("scorecard")
-                .add(scorecard)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(getApplicationContext(), "Score Added.", Toast.LENGTH_LONG).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getApplicationContext(), "Error"+e.getMessage(),Toast.LENGTH_LONG).show();
-                    }
-                });
-
-        Toast.makeText(getApplicationContext(), nameOfaPlayer,Toast.LENGTH_LONG).show();
-
     }
 
 
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            db = FirebaseFirestore.getInstance();
-
-            navigationView = findViewById(R.id.nav_view);
-            drawerLayout =  findViewById(R.id.drawer_layout);
-            toolbar=  (Toolbar) findViewById(R.id.toolBar);
-            setSupportActionBar(toolbar);
-            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    switch (item.getItemId()){
-                        case R.id.okay:
-                            item.setChecked(true);
-                            viewData();
-                            drawerLayout.closeDrawers();
-                            return true;
+        nameTv = findViewById(R.id.nameTv);
+        nameOfPlayer = findViewById(R.id.nameOfPlayer);
+        levelTv = findViewById(R.id.levelTv);
+        startButton = findViewById(R.id.startButton);
+        nav_view = findViewById(R.id.nav_view);
+        levelSpinner = findViewById(R.id.levelSpinner);
 
 
-                    }
-                    return false;
-                }
-            });
+        items = new String[]{"Select The Level---","Easy", "Medium", "Hard"};
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, items);
+        levelSpinner.setAdapter(adapter);
 
-            startButton = (Button)findViewById(R.id.startButton);
-            sumTextView = (TextView)findViewById(R.id.sumTextView);
-            button0 = (Button)findViewById(R.id.button0);
-            button1 = (Button)findViewById(R.id.button1);
-            button2 = (Button)findViewById(R.id.button2);
-            button3 = (Button)findViewById(R.id.button3);
-            resultTextView = (TextView) findViewById(R.id.resultTextView);
-            pointsTextView = (TextView) findViewById(R.id.pointsTextView);
-            timerTextView = (TextView) findViewById(R.id.timerTextView);
-            playAgainButton = (Button)findViewById(R.id.playAgainButton);
-            gameRelativeLayout = (RelativeLayout) findViewById(R.id.gameRelativeLayout);
-            nameOfPlayer = findViewById(R.id.nameOfPlayer);
-            dropdown = findViewById(R.id.levelSpinner);
-            nameTv=findViewById(R.id.nameTv);
-            levelTv = findViewById(R.id.levelTv);
-            items = new String[]{"select the level--","Easy", "Medium", "Hard"};
-            adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-            dropdown.setAdapter(adapter);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+
+        toggle.syncState();
 
 
 
+        //new functionality
+
+        secondSeekBar = findViewById(R.id.secondSeekBar);
+        showSecond = findViewById(R.id.showSeconds);
+        secondTv=findViewById(R.id.secondTv);
+        secondSeekBar.setMax(60);
+        secondSeekBar.setMin(10);
+
+
+
+        secondSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                showSecond.setText(i+"s");
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
-
-        private void setSupportActionBar(Toolbar toolbar) {
-
-        }
-
-        public void displayMessege(String messege){
-            Toast.makeText(this, messege, Toast.LENGTH_SHORT).show();
-
-        }
+    }
     }
 
 //this is falak
 //second time
+//harsh patel
