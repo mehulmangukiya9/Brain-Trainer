26b8cdc8fa06d291a5312523a6c120df4c999ed4
mehulmangukiya9 <mehulmangukiya777@gmail.com>
2021-01-18 02:12:43 -0500

    Flow-1 : Data successfully Send to the localhost(PhpMyAdmin).

--- app/src/main/java/com/example/braintrainerad/GenerateQuestion.java
+++ app/src/main/java/com/example/braintrainerad/GenerateQuestion.java
@@ -77,7 +77,7 @@ public class GenerateQuestion extends AppCompatActivity {
     int[] bigNumb;
 
     int gameTime;
-    public static final String ROOT_URL = "http://192.168.2.33/";
+    public static final String ROOT_URL = "http://192.168.2.18/";
 
 
     String levelChosenIntent;
@@ -348,7 +348,59 @@ public class GenerateQuestion extends AppCompatActivity {
 
     }
 
-    
+    private void insertUser(){
+        //Here we will handle the http request to insert user to mysql db
+        //Creating a RestAdapter
+        int gameTime2 = (gameTime-100)/1000;
+        RestAdapter adapter = new RestAdapter.Builder()
+                .setEndpoint(ROOT_URL) //Setting the Root URL
+                .build(); //Finally building the adapter
+
+        //Creating object for our interface
+        RegisterAPI api = adapter.create(RegisterAPI.class);
+
+        //Defining the method insertuser of our interface
+        api.insertUser(
+
+                //Passing the values by getting it from editTexts
+
+                name,
+                gameTime2,
+                score,
+
+                //Creating an anonymous callback
+                new Callback<Response>() {
+                    @Override
+                    public void success(Response result, Response response) {
+                        //On success we will read the server's output using bufferedreader
+                        //Creating a bufferedreader object
+                        BufferedReader reader = null;
+
+                        //An string to store output from the server
+                        String output = "";
+
+                        try {
+                            //Initializing buffered reader
+                            reader = new BufferedReader(new InputStreamReader(result.getBody().in()));
+
+                            //Reading the output in the string
+                            output = reader.readLine();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        //Displaying the output as a toast
+                        Toast.makeText(GenerateQuestion.this, output, Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        //If any error occured displaying the error as toast
+                        Toast.makeText(GenerateQuestion.this, error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
 
 
 
